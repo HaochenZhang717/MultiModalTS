9da7ad7d4622313dff36a3ed0558232e66f3089d
Haochen Zhang <hz112@rice.edu>
2026-02-28 12:05:44 -0600

    0228

--- scripts/synth_u/sample_qwen_my_embed.sh
+++ scripts/synth_u/sample_qwen_my_embed.sh
@@ -1,26 +1,6 @@
 export HF_HOME=/playpen/haochenz/hf_cache
-# todo: need to change
-#CUDA_VISIBLE_DEVICES=6 python sample_only.py \
-#    --cond_modal multimodal \
-#    --text_type my_generated_text_embeds \
-#    --training_stage finetune \
-#    --save_folder ../save/synth_u/use_qwen_my_embedding \
-#    --model_diff_config_path configs/synth_u/diff/model_text2ts_dep.yaml \
-#    --model_cond_config_path configs/synth_u/cond/text_msmdiffmv.yaml \
-#    --train_config_path configs/synth_u/train.yaml \
-#    --evaluate_config_path configs/synth_u/evaluate.yaml \
-#    --data_folder /playpen/haochenz/synthetic_u \
-#    --clip_folder "" \
-#    --multipatch_num 3 \
-#    --L_patch_len 3 \
-#    --base_patch 4 \
-#    --epochs 700 \
-#    --batch_size 512 \
-#    --clip_cache_path "" \
-#    --text_embeds_path /playpen/haochenz/diffusion_prior_results/DiTDH-S-samples_embed.pt \
-#    --seq_len 128
 
-CUDA_VISIBLE_DEVICES=6 python sample_only.py \
+CUDA_VISIBLE_DEVICES=6 python run.py \
     --cond_modal multimodal \
     --text_type my_generated_text_embeds \
     --training_stage finetune \
@@ -29,7 +9,7 @@ CUDA_VISIBLE_DEVICES=6 python sample_only.py \
     --model_cond_config_path configs/synth_u/cond/text_msmdiffmv.yaml \
     --train_config_path configs/synth_u/train.yaml \
     --evaluate_config_path configs/synth_u/evaluate.yaml \
-    --data_folder /playpen/haochenz/synthetic_u \
+    --data_folder /playpen/haochenz/diffusion_prior_results/for_sampling \
     --clip_folder "" \
     --multipatch_num 3 \
     --L_patch_len 3 \
@@ -37,5 +17,4 @@ CUDA_VISIBLE_DEVICES=6 python sample_only.py \
     --epochs 700 \
     --batch_size 512 \
     --clip_cache_path "" \
-    --text_embeds_path /playpen/haochenz/synthetic_u/test_embeds_my_caps.pt \
-    --seq_len 128
+    --only_evaluate true

85f0cb9f282d03e874517e55c6f2a9a5e89bfe1f
Haochen Zhang <hz112@rice.edu>
2026-03-01 14:35:17 -0600

    0301

--- analyze_results.py
+++ analyze_results.py
@@ -332,7 +332,7 @@ if __name__ == "__main__":
 
     calculate_all_scores_two_paths(
         real_path="/playpen/haochenz/synthetic_u/train_ts.npy",
-        fake_path="/playpen/haochenz/save/synth_u/use_qwen_my_embeds_v2/0/samples.pth"
+        fake_path="/playpen/haochenz/save/synth_u/qwen_my_embeds_v2/0/samples.pth"
     )
     # calculate_all_scores_given_real(real, "/playpen/haochenz/save/synth_u/use_qwen_my_embedding/0/sampled_ts.pth")
     # analyze_unconditional_results()

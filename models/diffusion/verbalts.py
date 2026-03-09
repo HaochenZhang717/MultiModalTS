e2d72c21a6cdaa540246f9b0cac1f53fd3bbbfb0
Haochen Zhang <hz112@rice.edu>
2026-03-09 17:35:48 -0500

    0309

--- models/diffusion/verbalts.py
+++ models/diffusion/verbalts.py
@@ -304,7 +304,7 @@ class VerbalTS(nn.Module):
         for layer in self.residual_layers:
             x_in, skip_connection = layer(x_in+_x_in, side_in, attr_emb, diffusion_emb, attention_mask=attention_mask, condition_type=self.config["condition_type"])
             skip.append(skip_connection)
-        breakpoint()
+
         x = torch.sum(torch.stack(skip), dim=0) / math.sqrt(len(self.residual_layers))
 
         x = x.reshape(B, self.channels, Nk * Nl)

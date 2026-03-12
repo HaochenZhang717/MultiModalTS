6b0d5e4bf35fea970116cfc6808821c1d911a67f
Haochen Zhang <hz112@rice.edu>
2026-03-12 01:58:38 -0500

    0312

--- scripts/synth_u/sample_qwen_my_embed.sh
+++ scripts/synth_u/sample_qwen_my_embed.sh
@@ -23,12 +23,12 @@ CUDA_VISIBLE_DEVICES=6 python run.py \
     --cond_modal multimodal \
     --text_type my_generated_text_embeds \
     --training_stage finetune \
-    --save_folder ../save/synth_u/qwen_my_embeds_v2 \
+    --save_folder ../save/synth_u/qwen_my_embeds_v2_0312 \
     --model_diff_config_path configs/synth_u/diff/model_text2ts_dep.yaml \
     --model_cond_config_path configs/synth_u/cond/text_msmdiffmv.yaml \
     --train_config_path configs/synth_u/train.yaml \
     --evaluate_config_path configs/synth_u/evaluate.yaml \
-    --data_folder /playpen/haochenz/dummy_dataset_for_sample_v2 \
+    --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/synthetic_u \
     --clip_folder "" \
     --multipatch_num 3 \
     --L_patch_len 3 \

6ae60bf4b7f445a9269fa645dc4400500c6e22ac
Haochen Zhang <hz112@rice.edu>
2026-03-13 19:48:27 -0500

    0313

--- scripts/synth_u_non_causal/train.sh
+++ scripts/synth_u_non_causal/train.sh
@@ -5,10 +5,10 @@ CUDA_VISIBLE_DEVICES=1 python run.py \
     --text_type my_generated_text_embeds \
     --training_stage finetune \
     --save_folder ../save/synth_u_non_causal/0313 \
-    --model_diff_config_path configs/synth_u/diff/model.yaml \
-    --model_cond_config_path configs/synth_u/cond/text_msmdiffmv.yaml \
-    --train_config_path configs/synth_u/train.yaml \
-    --evaluate_config_path configs/synth_u/evaluate.yaml \
+    --model_diff_config_path configs/synth_u_non_causal/diff/model.yaml \
+    --model_cond_config_path configs/synth_u_non_causal/cond/text_msmdiffmv.yaml \
+    --train_config_path configs/synth_u_non_causal/train.yaml \
+    --evaluate_config_path configs/synth_u_non_causal/evaluate.yaml \
     --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/synthetic_u \
     --clip_folder "" \
     --multipatch_num 3 \

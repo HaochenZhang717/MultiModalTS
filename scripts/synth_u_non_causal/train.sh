f77bafa0a5d4b1f021ecb1eb0a1c9e2d087ffddb
Haochen Zhang <hz112@rice.edu>
2026-03-13 20:40:13 -0500

    0313

--- scripts/synth_u_non_causal/train.sh
+++ scripts/synth_u_non_causal/train.sh
@@ -24,7 +24,7 @@ CUDA_VISIBLE_DEVICES=1 python run.py \
     --cond_modal multimodal \
     --text_type my_generated_text_embeds \
     --training_stage finetune \
-    --save_folder ../save/synth_u_non_causal/0313_prediction \
+    --save_folder ../save/synth_u_non_causal/0313_short_generation \
     --model_diff_config_path configs/synth_u_non_causal/diff/model_text2ts_dep.yaml \
     --model_cond_config_path configs/synth_u_non_causal/cond/text_msmdiffmv.yaml \
     --train_config_path configs/synth_u_non_causal/train.yaml \

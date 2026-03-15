6eaa16a2ab5a046bf3fb18078c70c30bb41e1287
Haochen Zhang <hz112@rice.edu>
2026-03-15 18:02:47 -0500

    0315

--- scripts/ablation/train_causal_no_text.sh
+++ scripts/ablation/train_causal_no_text.sh
@@ -6,12 +6,12 @@ CUDA_VISIBLE_DEVICES=5 python run_causal_no_text.py \
     --cond_modal multimodal \
     --text_type my_generated_text_embeds \
     --training_stage finetune \
-    --save_folder ../save/causal_correct/synth_m \
+    --save_folder ../save/causal_no_text/synth_m \
     --samples_name "samples.pt" \
-    --model_diff_config_path configs/synth_m_no_text/diff/model.yaml \
-    --model_cond_config_path configs/synth_m_no_text/cond/text_msmdiffmv.yaml \
-    --train_config_path configs/synth_m_no_text/train.yaml \
-    --evaluate_config_path configs/synth_m_no_text/evaluate.yaml \
+    --model_diff_config_path configs/synth_m_causal/diff/model.yaml \
+    --model_cond_config_path configs/synth_m_causal/cond/text_msmdiffmv.yaml \
+    --train_config_path configs/synth_m_causal/train.yaml \
+    --evaluate_config_path configs/synth_m_causal/evaluate.yaml \
     --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/synthetic_m \
     --clip_folder "" \
     --multipatch_num 3 \
@@ -29,10 +29,10 @@ CUDA_VISIBLE_DEVICES=5 python run_causal_no_text.py \
     --training_stage finetune \
     --save_folder ../save/causal_no_text/synth_u \
     --samples_name "samples.pt" \
-    --model_diff_config_path configs/synth_u_no_text/diff/model.yaml \
-    --model_cond_config_path configs/synth_u_no_text/cond/text_msmdiffmv.yaml \
-    --train_config_path configs/synth_u_no_text/train.yaml \
-    --evaluate_config_path configs/synth_u_no_text/evaluate.yaml \
+    --model_diff_config_path configs/synth_u_causal/diff/model.yaml \
+    --model_cond_config_path configs/synth_u_causal/cond/text_msmdiffmv.yaml \
+    --train_config_path configs/synth_u_causal/train.yaml \
+    --evaluate_config_path configs/synth_u_causal/evaluate.yaml \
     --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/synthetic_u \
     --clip_folder "" \
     --multipatch_num 3 \

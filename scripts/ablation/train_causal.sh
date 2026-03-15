4a8caabb526fa8d8d58f4cf63d86a6588a195d26
Haochen Zhang <hz112@rice.edu>
2026-03-14 23:47:41 -0500

    0313

--- scripts/ablation/train_causal.sh
+++ scripts/ablation/train_causal.sh
@@ -2,46 +2,46 @@ export HF_HOME=/playpen/haochenz/hf_cache
 export USE_CAUSAL="true"
 
 
-#CUDA_VISIBLE_DEVICES=5 python run_causal.py \
-#    --cond_modal multimodal \
-#    --text_type my_generated_text_embeds \
-#    --training_stage finetune \
-#    --save_folder ../save/causal_correct/synth_m \
-#    --samples_name "samples.pt" \
-#    --model_diff_config_path configs/synth_m_causal/diff/model.yaml \
-#    --model_cond_config_path configs/synth_m_causal/cond/text_msmdiffmv.yaml \
-#    --train_config_path configs/synth_m_causal/train.yaml \
-#    --evaluate_config_path configs/synth_m_causal/evaluate.yaml \
-#    --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/synthetic_m \
-#    --clip_folder "" \
-#    --multipatch_num 3 \
-#    --L_patch_len 2 \
-#    --base_patch 4 \
-#    --epochs 2500 \
-#    --batch_size 512 \
-#    --clip_cache_path "" \
-#    --n_runs 1
-#
-#
-#CUDA_VISIBLE_DEVICES=5 python run_causal.py \
-#    --cond_modal multimodal \
-#    --text_type my_generated_text_embeds \
-#    --training_stage finetune \
-#    --save_folder ../save/causal_correct/istanbul_traffic \
-#    --samples_name "samples.pt" \
-#    --model_diff_config_path configs/istanbul_traffic_causal/diff/model.yaml \
-#    --model_cond_config_path configs/istanbul_traffic_causal/cond/text_msmdiffmv.yaml \
-#    --train_config_path configs/istanbul_traffic_causal/train.yaml \
-#    --evaluate_config_path configs/istanbul_traffic_causal/evaluate.yaml \
-#    --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/istanbul_traffic \
-#    --clip_folder "" \
-#    --multipatch_num 3 \
-#    --L_patch_len 2 \
-#    --base_patch 4 \
-#    --epochs 2500 \
-#    --batch_size 512 \
-#    --clip_cache_path "" \
-#    --n_runs 1
+CUDA_VISIBLE_DEVICES=5 python run_causal.py \
+    --cond_modal multimodal \
+    --text_type my_generated_text_embeds \
+    --training_stage finetune \
+    --save_folder ../save/causal_correct/synth_m \
+    --samples_name "samples.pt" \
+    --model_diff_config_path configs/synth_m_causal/diff/model.yaml \
+    --model_cond_config_path configs/synth_m_causal/cond/text_msmdiffmv.yaml \
+    --train_config_path configs/synth_m_causal/train.yaml \
+    --evaluate_config_path configs/synth_m_causal/evaluate.yaml \
+    --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/synthetic_m \
+    --clip_folder "" \
+    --multipatch_num 3 \
+    --L_patch_len 2 \
+    --base_patch 4 \
+    --epochs 2500 \
+    --batch_size 512 \
+    --clip_cache_path "" \
+    --n_runs 1
+
+
+CUDA_VISIBLE_DEVICES=5 python run_causal.py \
+    --cond_modal multimodal \
+    --text_type my_generated_text_embeds \
+    --training_stage finetune \
+    --save_folder ../save/causal_correct/istanbul_traffic \
+    --samples_name "samples.pt" \
+    --model_diff_config_path configs/istanbul_traffic_causal/diff/model.yaml \
+    --model_cond_config_path configs/istanbul_traffic_causal/cond/text_msmdiffmv.yaml \
+    --train_config_path configs/istanbul_traffic_causal/train.yaml \
+    --evaluate_config_path configs/istanbul_traffic_causal/evaluate.yaml \
+    --data_folder /playpen/haochenz/LitsDatasets/128_len_ts/istanbul_traffic \
+    --clip_folder "" \
+    --multipatch_num 3 \
+    --L_patch_len 2 \
+    --base_patch 4 \
+    --epochs 2500 \
+    --batch_size 512 \
+    --clip_cache_path "" \
+    --n_runs 1
 
 
 CUDA_VISIBLE_DEVICES=5 python run_causal.py \

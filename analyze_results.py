dcdf1b70589a79114b62c5aeb49da5fa9dd471ce
Haochen Zhang <hz112@rice.edu>
2026-03-15 11:01:57 -0500

    0315

--- analyze_results.py
+++ analyze_results.py
@@ -1,240 +1,240 @@
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
-from metrics.discriminative_torch import discriminative_score_metrics
+from metrics.discriminative_torch import discriminative_score_metrics, moment_discriminative_score_metrics
 from metrics.predictive_metrics import predictive_score_metrics
 
 
-def visualize_sample(pt_path, idx=0):
-    data = torch.load(pt_path, map_location="cpu")
-
-    sample_dict = data[idx]
-    caption = sample_dict["caption"]
-    ts = sample_dict["sample"]  # Tensor
-    real = sample_dict["orig"]
-
-    if isinstance(ts, torch.Tensor):
-        ts = ts.numpy()
-
-    print("Caption:")
-    print(caption)
-    print("Shape:", ts.shape)
-    print("========"*30)
-
-    plt.figure(figsize=(10, 4))
-
-    # 如果是单通道
-    if ts.ndim == 1:
-        plt.plot(ts)
-    elif ts.ndim == 2:
-        # 多通道，每个通道画一条
-        for c in range(ts.shape[0]):
-            plt.plot(ts[c], label=f"Sample Channel {c}")
-
-    else:
-        raise ValueError("Unsupported time series shape")
-
-
-    if real.ndim == 1:
-        plt.plot(real)
-    elif real.ndim == 2:
-        # 多通道，每个通道画一条
-        for c in range(real.shape[0]):
-            plt.plot(real[c], label=f"real Channel {c}")
-
-    else:
-        raise ValueError("Unsupported time series shape")
-
-    plt.legend()
-    plt.title("Generated Time Series")
-    plt.xlabel("Time")
-    plt.ylabel("Value")
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
-
-
-def calculate_scores_from_real_language(pt_path):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    data = torch.load(pt_path, map_location=device)
-    real = []
-    fake = []
-    for datum in data:
-        real.append(datum["orig"])
-        fake.append(datum["sample"])
-    real = torch.stack(real)
-    fake = torch.stack(fake)
-
-    real = real.permute(0, 2, 1)
-    fake = fake.permute(0, 2, 1)
-    print("Real shape:", real.shape)
-    print("Fake shape:", fake.shape)
-    discriminative_score = discriminative_score_metrics(
-        real, fake,
-        real.shape[-1],
-        device,
-    )
-    print(f"Discriminative Score Metrics: {discriminative_score}")
-
-    predictive_score = predictive_score_metrics(
-        real, fake, device
-    )
-    print(f"Predictive Score Metrics: {predictive_score}")
-
-    return (
-        discriminative_score,
-        predictive_score,
-    )
-
-
-def calculate_scores_from_prior_language(sample_pt_path, real_pt_path):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    sample_data = torch.load(sample_pt_path, map_location=device)
-    fake = []
-    for sample_datum in sample_data:
-        fake.append(sample_datum["sample"])
-    fake = torch.stack(fake)
-
-    real_data = torch.load(real_pt_path, map_location=device)
-    real = []
-    for real_datum in real_data:
-        real.append(real_datum["orig"])
-    real = torch.stack(real)
-
-
-    real = real.permute(0, 2, 1)
-    fake = fake.permute(0, 2, 1)
-    print("Real shape:", real.shape)
-    print("Fake shape:", fake.shape)
-    discriminative_score = discriminative_score_metrics(
-        real, fake,
-        real.shape[-1],
-        device,
-    )
-    print(f"Discriminative Score Metrics: {discriminative_score}")
-
-    predictive_score = predictive_score_metrics(
-        real, fake, device
-    )
-    print(f"Predictive Score Metrics: {predictive_score}")
-
-    return (
-        discriminative_score,
-        predictive_score,
-    )
-
-
-def calculate_scores_unconditional(sample_pt_path, real_pt_path):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    fake = torch.load(sample_pt_path, map_location=device)
-
-    real_data = torch.load(real_pt_path, map_location=device)
-    real = []
-    for real_datum in real_data:
-        real.append(real_datum["orig"])
-    real = torch.stack(real)
-
-
-    real = real.permute(0, 2, 1)
-    fake = fake.permute(0, 2, 1)
-    print("Real shape:", real.shape)
-    print("Fake shape:", fake.shape)
-    discriminative_score = discriminative_score_metrics(
-        real, fake,
-        real.shape[-1],
-        device,
-    )
-    print(f"Discriminative Score Metrics: {discriminative_score}")
-
-    predictive_score = predictive_score_metrics(
-        real, fake, device
-    )
-    print(f"Predictive Score Metrics: {predictive_score}")
-
-    return (
-        discriminative_score,
-        predictive_score,
-    )
-
-
-def analyze_text2ts_results():
-    disc_score_list = []
-    pred_score_list = []
-    for _ in range(20):
-        # disc_score, pred_score = calculate_scores_from_prior_language(
-        #     sample_pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S/text2ts_from_language_prior_results.pt",
-        #     real_pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S/text2ts_results.pt"
-        # )
-        disc_score, pred_score = calculate_scores_from_real_language(
-            pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S/text2ts_results.pt")
-
-        disc_score_list.append(disc_score)
-        pred_score_list.append(pred_score)
-
-    disc_score_arr = np.array(disc_score_list)
-    pred_score_arr = np.array(pred_score_list)
-
-    disc_mean = disc_score_arr.mean()
-    disc_std = disc_score_arr.std(ddof=1)  # sample std
-
-    pred_mean = pred_score_arr.mean()
-    pred_std = pred_score_arr.std(ddof=1)
-
-    print(f"Disc Score: mean = {disc_mean:.4f}, std = {disc_std:.4f}")
-    print(f"Pred Score: mean = {pred_mean:.4f}, std = {pred_std:.4f}")
-
-
-def analyze_text2ts_results_from_prior_language():
-    disc_score_list = []
-    pred_score_list = []
-    for _ in range(20):
-        disc_score, pred_score = calculate_scores_from_prior_language(
-            sample_pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S/text2ts_from_language_prior_results.pt",
-            real_pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S/text2ts_results.pt"
-        )
-
-        disc_score_list.append(disc_score)
-        pred_score_list.append(pred_score)
-
-    disc_score_arr = np.array(disc_score_list)
-    pred_score_arr = np.array(pred_score_list)
-
-    disc_mean = disc_score_arr.mean()
-    disc_std = disc_score_arr.std(ddof=1)  # sample std
-
-    pred_mean = pred_score_arr.mean()
-    pred_std = pred_score_arr.std(ddof=1)
-
-    print(f"Disc Score: mean = {disc_mean:.4f}, std = {disc_std:.4f}")
-    print(f"Pred Score: mean = {pred_mean:.4f}, std = {pred_std:.4f}")
+# def visualize_sample(pt_path, idx=0):
+#     data = torch.load(pt_path, map_location="cpu")
+#
+#     sample_dict = data[idx]
+#     caption = sample_dict["caption"]
+#     ts = sample_dict["sample"]  # Tensor
+#     real = sample_dict["orig"]
+#
+#     if isinstance(ts, torch.Tensor):
+#         ts = ts.numpy()
+#
+#     print("Caption:")
+#     print(caption)
+#     print("Shape:", ts.shape)
+#     print("========"*30)
+#
+#     plt.figure(figsize=(10, 4))
+#
+#     # 如果是单通道
+#     if ts.ndim == 1:
+#         plt.plot(ts)
+#     elif ts.ndim == 2:
+#         # 多通道，每个通道画一条
+#         for c in range(ts.shape[0]):
+#             plt.plot(ts[c], label=f"Sample Channel {c}")
+#
+#     else:
+#         raise ValueError("Unsupported time series shape")
+#
+#
+#     if real.ndim == 1:
+#         plt.plot(real)
+#     elif real.ndim == 2:
+#         # 多通道，每个通道画一条
+#         for c in range(real.shape[0]):
+#             plt.plot(real[c], label=f"real Channel {c}")
+#
+#     else:
+#         raise ValueError("Unsupported time series shape")
+#
+#     plt.legend()
+#     plt.title("Generated Time Series")
+#     plt.xlabel("Time")
+#     plt.ylabel("Value")
+#     plt.grid(True)
+#     plt.tight_layout()
+#     plt.show()
+#
+#
+#
+# def calculate_scores_from_real_language(pt_path):
+#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#     data = torch.load(pt_path, map_location=device)
+#     real = []
+#     fake = []
+#     for datum in data:
+#         real.append(datum["orig"])
+#         fake.append(datum["sample"])
+#     real = torch.stack(real)
+#     fake = torch.stack(fake)
+#
+#     real = real.permute(0, 2, 1)
+#     fake = fake.permute(0, 2, 1)
+#     print("Real shape:", real.shape)
+#     print("Fake shape:", fake.shape)
+#     discriminative_score = discriminative_score_metrics(
+#         real, fake,
+#         real.shape[-1],
+#         device,
+#     )
+#     print(f"Discriminative Score Metrics: {discriminative_score}")
+#
+#     predictive_score = predictive_score_metrics(
+#         real, fake, device
+#     )
+#     print(f"Predictive Score Metrics: {predictive_score}")
+#
+#     return (
+#         discriminative_score,
+#         predictive_score,
+#     )
+#
+#
+# def calculate_scores_from_prior_language(sample_pt_path, real_pt_path):
+#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#
+#     sample_data = torch.load(sample_pt_path, map_location=device)
+#     fake = []
+#     for sample_datum in sample_data:
+#         fake.append(sample_datum["sample"])
+#     fake = torch.stack(fake)
+#
+#     real_data = torch.load(real_pt_path, map_location=device)
+#     real = []
+#     for real_datum in real_data:
+#         real.append(real_datum["orig"])
+#     real = torch.stack(real)
+#
+#
+#     real = real.permute(0, 2, 1)
+#     fake = fake.permute(0, 2, 1)
+#     print("Real shape:", real.shape)
+#     print("Fake shape:", fake.shape)
+#     discriminative_score = discriminative_score_metrics(
+#         real, fake,
+#         real.shape[-1],
+#         device,
+#     )
+#     print(f"Discriminative Score Metrics: {discriminative_score}")
+#
+#     predictive_score = predictive_score_metrics(
+#         real, fake, device
+#     )
+#     print(f"Predictive Score Metrics: {predictive_score}")
+#
+#     return (
+#         discriminative_score,
+#         predictive_score,
+#     )
+#
+#
+# def calculate_scores_unconditional(sample_pt_path, real_pt_path):
+#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#
+#     fake = torch.load(sample_pt_path, map_location=device)
+#
+#     real_data = torch.load(real_pt_path, map_location=device)
+#     real = []
+#     for real_datum in real_data:
+#         real.append(real_datum["orig"])
+#     real = torch.stack(real)
+#
+#
+#     real = real.permute(0, 2, 1)
+#     fake = fake.permute(0, 2, 1)
+#     print("Real shape:", real.shape)
+#     print("Fake shape:", fake.shape)
+#     discriminative_score = discriminative_score_metrics(
+#         real, fake,
+#         real.shape[-1],
+#         device,
+#     )
+#     print(f"Discriminative Score Metrics: {discriminative_score}")
+#
+#     predictive_score = predictive_score_metrics(
+#         real, fake, device
+#     )
+#     print(f"Predictive Score Metrics: {predictive_score}")
+#
+#     return (
+#         discriminative_score,
+#         predictive_score,
+#     )
+#
+#
+# def analyze_text2ts_results():
+#     disc_score_list = []
+#     pred_score_list = []
+#     for _ in range(20):
+#         # disc_score, pred_score = calculate_scores_from_prior_language(
+#         #     sample_pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S/text2ts_from_language_prior_results.pt",
+#         #     real_pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S/text2ts_results.pt"
+#         # )
+#         disc_score, pred_score = calculate_scores_from_real_language(
+#             pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S/text2ts_results.pt")
+#
+#         disc_score_list.append(disc_score)
+#         pred_score_list.append(pred_score)
+#
+#     disc_score_arr = np.array(disc_score_list)
+#     pred_score_arr = np.array(pred_score_list)
+#
+#     disc_mean = disc_score_arr.mean()
+#     disc_std = disc_score_arr.std(ddof=1)  # sample std
+#
+#     pred_mean = pred_score_arr.mean()
+#     pred_std = pred_score_arr.std(ddof=1)
+#
+#     print(f"Disc Score: mean = {disc_mean:.4f}, std = {disc_std:.4f}")
+#     print(f"Pred Score: mean = {pred_mean:.4f}, std = {pred_std:.4f}")
 
 
-def analyze_unconditional_results():
-    disc_score_list = []
-    pred_score_list = []
-    for _ in range(20):
-        disc_score, pred_score = calculate_scores_unconditional(
-            sample_pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S-BS512-LR2e-4-unconditional/uncond_samples.pt",
-            real_pt_path="text2ts_results.pt"
-        )
-
-        disc_score_list.append(disc_score)
-        pred_score_list.append(pred_score)
-
-    disc_score_arr = np.array(disc_score_list)
-    pred_score_arr = np.array(pred_score_list)
-
-    disc_mean = disc_score_arr.mean()
-    disc_std = disc_score_arr.std(ddof=1)  # sample std
-
-    pred_mean = pred_score_arr.mean()
-    pred_std = pred_score_arr.std(ddof=1)
-
-    print(f"Disc Score: mean = {disc_mean:.4f}, std = {disc_std:.4f}")
-    print(f"Pred Score: mean = {pred_mean:.4f}, std = {pred_std:.4f}")
+# def analyze_text2ts_results_from_prior_language():
+#     disc_score_list = []
+#     pred_score_list = []
+#     for _ in range(20):
+#         disc_score, pred_score = calculate_scores_from_prior_language(
+#             sample_pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S/text2ts_from_language_prior_results.pt",
+#             real_pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S/text2ts_results.pt"
+#         )
+#
+#         disc_score_list.append(disc_score)
+#         pred_score_list.append(pred_score)
+#
+#     disc_score_arr = np.array(disc_score_list)
+#     pred_score_arr = np.array(pred_score_list)
+#
+#     disc_mean = disc_score_arr.mean()
+#     disc_std = disc_score_arr.std(ddof=1)  # sample std
+#
+#     pred_mean = pred_score_arr.mean()
+#     pred_std = pred_score_arr.std(ddof=1)
+#
+#     print(f"Disc Score: mean = {disc_mean:.4f}, std = {disc_std:.4f}")
+#     print(f"Pred Score: mean = {pred_mean:.4f}, std = {pred_std:.4f}")
+#
+#
+# def analyze_unconditional_results():
+#     disc_score_list = []
+#     pred_score_list = []
+#     for _ in range(20):
+#         disc_score, pred_score = calculate_scores_unconditional(
+#             sample_pt_path="/playpen/haochenz/ckpts_ts_generation/DiTDH-S-BS512-LR2e-4-unconditional/uncond_samples.pt",
+#             real_pt_path="text2ts_results.pt"
+#         )
+#
+#         disc_score_list.append(disc_score)
+#         pred_score_list.append(pred_score)
+#
+#     disc_score_arr = np.array(disc_score_list)
+#     pred_score_arr = np.array(pred_score_list)
+#
+#     disc_mean = disc_score_arr.mean()
+#     disc_std = disc_score_arr.std(ddof=1)  # sample std
+#
+#     pred_mean = pred_score_arr.mean()
+#     pred_std = pred_score_arr.std(ddof=1)
+#
+#     print(f"Disc Score: mean = {disc_mean:.4f}, std = {disc_std:.4f}")
+#     print(f"Pred Score: mean = {pred_mean:.4f}, std = {pred_std:.4f}")
 
 
 def calculate_all_scores(results_path, block_id):
@@ -255,7 +255,7 @@ def calculate_all_scores(results_path, block_id):
         fake = results_dict["sampled_ts"][i][:,:,pred_start:pred_end]
         # print(f"real: {real.shape}, fake: {fake.shape}")
         # breakpoint()
-        discriminative_score = discriminative_score_metrics(
+        discriminative_score = moment_discriminative_score_metrics(
             real, fake,
             real.shape[-1],
             device,

cb4a43f9c6ec4af3e0d806d4173ff87c15695eb9
Haochen Zhang <hz112@rice.edu>
2026-03-16 00:52:44 -0500

    0315

--- scripts/aireadi/train_aireadi.sh
+++ scripts/aireadi/train_aireadi.sh
@@ -2,25 +2,26 @@ export CUDA_VISIBLE_DEVICES=7
 export USE_CAUSAL="true"
 
 
-python run_forecast.py \
-    --cond_modal aireadi \
-    --text_type my_generated_text_embeds \
-    --training_stage finetune \
-    --save_folder ../save/aireadi/history_0315 \
-    --samples_name "samples.pt" \
-    --model_diff_config_path configs/aireadi/diff/model_text2ts_dep.yaml \
-    --model_cond_config_path configs/aireadi/cond/text_msmdiffmv.yaml \
-    --train_config_path configs/aireadi/train.yaml \
-    --evaluate_config_path configs/aireadi/evaluate.yaml \
-    --data_folder "none" \
-    --clip_folder "" \
-    --multipatch_num 3 \
-    --L_patch_len 2 \
-    --base_patch 4 \
-    --epochs 700 \
-    --batch_size 512 \
-    --clip_cache_path "" \
-    --condition_type "history"
+#python run_forecast.py \
+#    --cond_modal aireadi \
+#    --text_type my_generated_text_embeds \
+#    --training_stage finetune \
+#    --save_folder ../save/aireadi/history_0315 \
+#    --samples_name "samples.pt" \
+#    --model_diff_config_path configs/aireadi/diff/model_text2ts_dep.yaml \
+#    --model_cond_config_path configs/aireadi/cond/text_msmdiffmv.yaml \
+#    --train_config_path configs/aireadi/train.yaml \
+#    --evaluate_config_path configs/aireadi/evaluate.yaml \
+#    --data_folder "none" \
+#    --clip_folder "" \
+#    --multipatch_num 3 \
+#    --L_patch_len 2 \
+#    --base_patch 4 \
+#    --epochs 700 \
+#    --batch_size 512 \
+#    --clip_cache_path "" \
+#    --n_runs 1 \
+#    --condition_type "history"
 
 
 python run_forecast.py \
@@ -41,6 +42,7 @@ python run_forecast.py \
     --epochs 700 \
     --batch_size 512 \
     --clip_cache_path "" \
+    --n_runs 1 \
     --condition_type "adaLN"
 
 

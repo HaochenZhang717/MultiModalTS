efbb9bcf9dfc70a3eba4e82280fa6ce8c0420f9f
Haochen Zhang <hz112@rice.edu>
2026-03-17 10:30:30 -0500

    0316

--- analyze_results.py
+++ analyze_results.py
@@ -461,7 +461,7 @@ def calculate_all_scores_two_paths(real_path, fake_path):
     samples_dict = torch.load(fake_path, map_location="cpu", weights_only=False)
     print(f"fake shape = {samples_dict['sampled_ts'].shape}")
     num_samples = min(len(real), len(samples_dict["sampled_ts"][0]))
-    real = real[:num_samples]
+    real = real[:num_samples].permute(0,2,1)
 
 
     print(f"real shape: {real.shape}")
@@ -483,7 +483,7 @@ def calculate_all_scores_two_paths(real_path, fake_path):
     real_emb = _moment_embed(moment_model, real, device)
 
     for i in range(10):
-        fake = samples_dict["sampled_ts"][i, :num_samples]
+        fake = samples_dict["sampled_ts"][i, :num_samples].permute(0,2,1)
 
         discriminative_score = discriminative_score_metrics(
             real, fake,
